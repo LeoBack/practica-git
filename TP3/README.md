612cbe8da30ca932df3eac0914c121142ba721e9
Leonardo <leonardo.back@outlook.com>
2023-02-08 22:19:38 -0300

    TP3 Completado

---
+++ TP3/README.md
@@ -0,0 +1,37 @@
+### TP 4
+1) Escribir una función a la que se le pase una cadena "nombre" y muestre por pantalla el saludo ¡hola nombre!.
+
+2) Escribir un programa que le pida al usuario que ingrese su nombre apellido edad y muestre un objeto con los datos ingresados.
+
+Ej:
+```json
+{
+  nombre: "",
+  apellido: "",
+  edad: 0
+}
+```
+
+3) Escribir una funcion que espere dos parámetros, uno de los cuales deberá ser un array, y el otro parámetro debe ser el elemento a buscar en el array. Si lo encuentra debera devoler "Elemento encontrado en la posicion (posición)".
+
+Ej.
+```js
+const miLista1 = [2, "Eric", "Jarra"]
+const miLista2 = ["Leche", "Café", "Chocolate"]
+function buscarElemento() {
+  // aqui defino la funcion
+}
+buscarElemento(miLista1, "Jarra")
+// Esta funcion debe retornar:
+// Elemento encontrado en la posicion 2
+buscarElemento(miLista2, "Café")
+// Esta funcion debe retornar:
+// Elemento encontrado en la posicion 1
+```
+
+4) Dada un lista o array, escribir un programa que invierta sus elementos.
+
+Ej
+```
+ [3,5,7,8]  // ---->  [8,7,5,3]
+```
---
+++ TP3/index.js
@@ -0,0 +1,97 @@
+console.log("-TP3------------------");
+const readlineSync = require("readline-sync")
+// 1) Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+console.log("-#1------------------");
+const userName = readlineSync.question("Ingrese su nombre: ")
+
+for (i = 0; i < 10;) {
+    console.log("#", ++i, " Hola como estas ", userName)
+}
+
+// 2) Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+console.log("-#2------------------");
+
+function printEdad(pEdad) {
+    for (i = 0; i < pEdad;) {
+        console.log("Edad # ", ++i)
+    }
+    //for (i = 1; i < pEdad+1; i++) {
+    //    console.log("Edad # ", i)
+    //}  
+}
+
+const edad = readlineSync.question("Ingrese su edad: ")
+printEdad(edad );
+
+// 3) Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.
+console.log("-#3------------------");
+
+const esEntero = (x) => (~~x === x)
+const esNumeroImpar = (pNum) => (pNum % 2)
+
+function print(pNum) {
+    let numeros = ""
+    for (i = 0; i < pNum; i++) {
+        if (i === 1) {
+            numeros = i
+        }
+        else if (esNumeroImpar(i)) {
+            numeros = numeros + ", " + i
+        }
+        else { }
+    }
+    return numeros
+}
+
+const num = readlineSync.question("Ingrese un numero entero positivo: ")
+
+if (esEntero(num)) {
+    console.log("El numero no un entero")
+}
+else if (num < 0) {
+    console.log("El numero no es positivo")
+}
+else {
+    console.log(print(num))
+}
+
+// 4) Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectángulo como el de más abajo, de altura el número introducido.
+// ```
+// *
+// **
+// ***
+// ****
+// *****
+// ```
+console.log("-#4------------------");
+const numEntero = readlineSync.question("Ingrese un numero entero: ")
+
+function asteriscos(pNum) {
+    let result = " "
+    for (i = 0; i < pNum; i++) {
+        result = result + "*"
+    }
+    return result
+} 
+
+function printAsteriscos(pNum) {
+    for (i = 0; i < pNum;) {
+        console.log(asteriscos(++i))
+    }
+}
+
+printAsteriscos(numEntero)
+
+//5) Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
+console.log("-#5------------------");
+const palabra = readlineSync.question("Ingrese una palabra: ")
+
+function printAsterisco(pPalabra) {
+    let resultPalabra = " "
+    for (i = pPalabra.length-1; i >= 0; i--) {
+        resultPalabra = resultPalabra + pPalabra[i]
+    }
+    return resultPalabra
+}
+
+console.log(printAsterisco(palabra))
