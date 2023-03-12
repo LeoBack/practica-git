5acd4511571ccf72027a435868ab2399b6aaec3e
Leonardo <leonardo.back@outlook.com>
2023-03-11 23:36:39 -0300

    add setpoints TP5 and TP6

---
+++ TP5/README.md
@@ -0,0 +1,15 @@
+### TP 5
+#### Arrays
+
+1) Dados 2 arrays **["Juan", "Dani"]** y **["Leo", "Monica", "Juliana"]**, escribir una función que concatene "unifique" los dos array y devuelva un solo array **["Juan", "Dani", "Leo", "Monica", "Juliana"]**. 
+
+- Ayuda: Puedes crear un tercer array vacío e ir poniendo los elementos en dicho array.
+
+2) Escriba una función con el nombre de esVocal() que tome un carácter , devuelva True si es vocal (no importa si es mayúscula o minúscula), y devuelva False en caso contrario.
+
+3) Crea una función llamada generar_caracteres() que tome como parámetro un número entero (n) y un carácter, retornar el carácter multiplicado por n. Por ejemplo, generar_caracteres(5,x), debería retornar «xxxxx».
+
+4) Crear una función sumaArreglo() que tome como parámetro un arreglo de números, retornar la suma de todos los números del arreglo.
+
+5) Crea una función llamada numeroMayor() que toma tres números como entrada y retornar el numero mayor de ellos, si son iguales devolver un String «son iguales».
+---
---
+++ TP5/index.js
@@ -0,0 +1,21 @@
+// 1) Dados 2 arrays **["Juan", "Dani"]** y **["Leo", "Monica", "Juliana"]**, 
+// escribir una función que concatene "unifique" los dos array y devuelva 
+// un solo array **["Juan", "Dani", "Leo", "Monica", "Juliana"]**. 
+// - ayuda: Puedes crear un tercer array vacío e ir poniendo los elementos en dicho array.
+console.log("-#1------------------");
+
+// 2) Escriba una función con el nombre de esVocal() que tome un carácter , 
+// devuelva True si es vocal (no importa si es mayúscula o minúscula), y devuelva False en caso contrario.
+console.log("-#2------------------");
+
+// 3) Crea una función llamada generar_caracteres() que tome como parámetro un número entero (n) 
+// y un carácter, retornar el carácter multiplicado por n. Por ejemplo, generar_caracteres(5,x), debería retornar «xxxxx».
+console.log("-#3------------------");
+
+// 4) Crear una función sumaArreglo() que tome como parámetro un arreglo de números, 
+// retornar la suma de todos los números del arreglo.
+console.log("-#4------------------");
+
+// 5) Crea una función llamada numeroMayor() que toma tres números como entrada y 
+// retornar el numero mayor de ellos, si son iguales devolver un String «son iguales».
+console.log("-#5------------------");
